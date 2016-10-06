aa96e514fc0e4f21ca321de39c66009fa3887abd
David Barnes <barnesdavidj@gmail.com>
2016-10-06 17:27:40 -0400

    Finish the inclass with a employee collection and using polymorphism.

---
+++ cis237inclass3/EmployeeCollection.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace cis237inclass3
+{
+    class EmployeeCollection : IEmployeeCollection
+    {
+        //I can instanciate this array as a Employee type because it is an arary.
+        //Even thoug I can't instanciate a single Employee since it is abstact, and array
+        //is fine. I just have to make sure that I put non-abstract instances into it.
+        IEmployee[] employees = new Employee[10];
+
+        int currentIndex;
+
+        //Constructor
+        public EmployeeCollection()
+        {
+            currentIndex = 0;
+        }
+
+        public void add(string FirstName, string LastName, DateTime StartDate, decimal Salary)
+        {
+            employees[currentIndex++] = new SalaryEmployee(FirstName, LastName, StartDate, Salary);
+        }
+
+        public void add(string FirstName, string LastName, DateTime StartDate, decimal HourlyRate, decimal HoursPerWeek)
+        {
+            employees[currentIndex++] = new HourlyEmployee(FirstName, LastName, StartDate, HourlyRate, HoursPerWeek);
+        }
+
+        public string GetPrintString()
+        {
+            //declare a blank string to start
+            string allOutput = "";
+
+            //For each employee in the collection
+            foreach (Employee employee in employees)
+            {
+                //So long as the employee is not null
+                if (employee != null)
+                {
+                    //Concat the employee information
+                    allOutput += employee.ToString() + Environment.NewLine;
+
+                    //Right into the Danger Zone!!!!
+                    //We need to check to see if the type is of SalaryEmployee
+                    //otherwise, calling the NameAndSalary Method would fail for
+                    //all other types of classes.
+                    if (employee.GetType() == typeof(SalaryEmployee))
+                    {
+                        //This is called a down cast because we are down casting it from
+                        //the type of Employee down to the type of SalaryEmployee
+                        allOutput += ((SalaryEmployee)employee).NameAndSalary() + Environment.NewLine;
+                    }                   
+                }
+            }
+            //Return the concated string
+            return allOutput;
+        }
+    }
+}
--- cis237inclass3/Program.cs
+++ cis237inclass3/Program.cs
@@ -20,8 +20,32 @@ namespace cis237inclass3
             Console.WriteLine(hEmployee.ToString());
             Console.WriteLine(hEmployee.GetYearlySalary());
             Console.WriteLine(hEmployee.GetAllEmployeeInformation());
-            
-            
+
+            //Declare a new colletion
+            IEmployeeCollection employeeCollection;
+            //Instanciate the collection with the concreate class even though it is going into
+            //an interface container
+            employeeCollection = new EmployeeCollection();
+            //Call the method below to add the employees to the collection
+            AddNewEmployees(employeeCollection);
+
+            //Print the employees out
+            PrintEmployees(employeeCollection);
+        }
+
+        static void AddNewEmployees(IEmployeeCollection employeeCollection)
+        {
+            //Add a couple of Salary Employees
+            employeeCollection.add("James", "Kirk", new DateTime(2016, 4, 4), 549m);
+            employeeCollection.add("Jean-Luc", "Picard", new DateTime(2016, 5, 14), 609m);
+            //Add a couple of Hourly Employees
+            employeeCollection.add("Benjamin", "Sisko", new DateTime(2016, 4, 4), 49m, 40);
+            employeeCollection.add("Johnathan", "Archer", new DateTime(2016, 4, 4), 54m, 20);
+        }
+
+        static void PrintEmployees(IEmployeeCollection employeeCollection)
+        {
+            Console.WriteLine(employeeCollection.GetPrintString());
         }
     }
 }
